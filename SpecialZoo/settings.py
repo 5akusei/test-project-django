9e55d32f74ee245a58ee7d00e04dea74c6e9269a
Jeison <jeisonmulti@gmail.com>
2022-03-09 21:09:35 -0600

    Add folder templates

--- SpecialZoo/settings.py
+++ SpecialZoo/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'SpecialZoo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates', 'templates/layouts'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
