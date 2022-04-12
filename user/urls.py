3778ba3ac02070236628c56fa07234074428147d
Jeison <jeisonmulti@gmail.com>
2022-04-11 20:50:41 -0600

    implement back link

--- user/urls.py
+++ user/urls.py
@@ -1,17 +1,18 @@
 # from django import views
 from django.urls import path
-from user.views import CreateRecord, ListRecord, UpdateRecord, DeleteRecord
+from user.views import CreateRecord, ListRecord, UpdateRecord, DeleteRecord, Index
 from django.contrib.auth.views import LogoutView
 from . import views
 from SpecialZoo import settings
 app_name = 'user'
 
 urlpatterns = [
-    path('', views.login_page, name='login'),
-    path('registro', CreateRecord.as_view(), name='signup'),
-    # path('cerrar-sesion/', views.logout, name='logout'),
+    path('', Index.as_view(), name='index'),
+    path('iniciar-sesion/', views.login_page, name='login'),
     path('cerrar-sesion/', LogoutView.as_view(), name='logout'),
+    # path('cerrar-sesion/', views.logout, name='logout'),
     path('lista', ListRecord.as_view(), name='list'),
-    path('actualizar/<int:pk>', UpdateRecord.as_view(), name='update'),
+    path('registro', CreateRecord.as_view(), name='signup'),
     path('eliminar/<int:pk>', DeleteRecord.as_view(), name='delete'),
+    path('actualizar/<int:pk>', UpdateRecord.as_view(), name='update'),
 ]
--- user/views.py
+++ user/views.py
@@ -3,10 +3,33 @@ from pprint import pprint
 from django.contrib import messages
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, TemplateView
 from user.forms import UserForm, UserFormUpdate, LoginForm
 from user.models import User
 from django.contrib.auth import login, logout, authenticate
+
+class ListRecord(ListView):
+    queryset = User.objects.order_by('date_joined')
+    template_name = 'user/list.html'
+    context_object_name = 'records'
+
+    # Validate user
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            if request.method == 'GET':
+                return super().dispatch(request, *args, **kwargs)
+            elif request.method == 'POST':
+                messages.error(request, 'Acción invalida.')
+                return redirect('user:login')    
+        else:
+            messages.error(request, 'Por favor inicia sesión e intenta de nuevo.')
+            return redirect('user:login')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = UserForm
+    
+        return context
 
 class CreateRecord(CreateView):
     template_name = 'user/sign_up.html'
@@ -41,39 +64,25 @@ class CreateRecord(CreateView):
         messages.error(self.request, "Los datos proporcionados son invalidos")
         return redirect('user:signup')
 
-class ListRecord(ListView):
-    queryset = User.objects.order_by('date_joined')
-    template_name = 'user/list.html'
-    context_object_name = 'records'
-
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            if request.method == 'GET':
-                return super().dispatch(request, *args, **kwargs)
-            elif request.method == 'POST':
-                messages.error(request, 'Acción invalida.')
-                return redirect('user:login')    
-        else:
-            messages.error(request, 'Por favor inicia sesión e intenta de nuevo.')
-            return redirect('user:login')
-        # if request.method.lower() in self.http_method_names:
-        #     handler = getattr(self, request.method.lower(), self.http_method_not_allowed)
-        # else:
-        #     handler = self.http_method_not_allowed
-        # return handler(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = UserForm
-    
-        return context
-
 class UpdateRecord(UpdateView):
     template_name = 'user/update.html'
     model = User
     form_class = UserFormUpdate
     context_object_name = 'form'
     success_url = reverse_lazy('user:list')
+
+    # Validate user
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            # pdb.set_trace()
+            if request.user.is_admin or request.user.id == self.kwargs.get('pk'):
+                return super().dispatch(request, *args, **kwargs)
+            else:
+                messages.error(request, 'No estas autorizado.')
+                return redirect('user:index')    
+        else:
+            messages.error(request, 'Por favor inicia sesión e intenta de nuevo.')
+            return redirect('user:login')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -91,28 +100,57 @@ class UpdateRecord(UpdateView):
 
 class DeleteRecord(DeleteView):
     model = User
-    success_url = reverse_lazy('user:list')
+    success_url = reverse_lazy('user:list') 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            if request.user.is_admin:
+                return super().dispatch(request, *args, **kwargs)
+            else:
+                messages.error(request, 'No estas autorizado.')
+                return redirect('user:index')
+        else:
+            messages.error(request, 'Por favor inicia sesión e intenta de nuevo.')
+            return redirect('user:login')
 
 def logout_view(request):
-    logout(request)
-    messages.success(request, 'Sesión Cerrada')
-    return redirect('user:login')
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request, 'Sesión Cerrada')
+        return redirect('user:login')
 
 def login_page(request):
     form = LoginForm()
     message = ''
-    context={'form': form, 'message': message}
-    if request.method == 'POST':
-        
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            user = authenticate(
-                username = form.cleaned_data['email'],
-                password = form.cleaned_data['password'],
-            )
-            # pdb.set_trace()
-            if user is not None:
-                login(request, user)
-                return redirect('zoo:home')
-        message = 'Login failed!'
-    return render(request, 'user/login.html', context)
+    context = {'form': form, 'message': message}
+    # Validate user
+    if request.user.is_authenticated:
+        return redirect('user:index')
+    else:
+        if request.method == 'POST':
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                user = authenticate(
+                    username = form.cleaned_data['email'],
+                    password = form.cleaned_data['password'],
+                )
+                # pdb.set_trace()
+                if user is not None:
+                    login(request, user)
+                    return redirect('user:index')
+                message = 'Login failed!'
+        return render(request, 'user/login.html', context)
+
+class Index(TemplateView):
+    template_name = 'user/index.html'
+
+    # Validate user
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            if request.method == 'GET':
+                return super().dispatch(request, *args, **kwargs)
+            elif request.method == 'POST':
+                messages.error(request, 'Acción invalida. ¡Inicia Sesión!')
+                return redirect('user:index')    
+        else:
+            messages.error(request, 'Por favor inicia sesión e intenta de nuevo.')
+            return redirect('user:login')
